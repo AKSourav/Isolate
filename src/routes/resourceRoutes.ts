6bc3950d3483d95a2904e6921b98ffd129eb7fda
Anupam Kumar Sourav <anupam.sony32@gmail.com>
2024-11-20 00:16:28 +0530

    route to create a service

--- src/routes/resourceRoutes.ts
+++ src/routes/resourceRoutes.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
 import {list_services} from '../controllers/resourceControllers';
+import {create_service} from '../controllers/serviceControllers';
 import {protect} from "../middleware/authMiddleware";
 
 const router = express.Router()
 
-router.route('/').get(list_services);
+router.route('/').get(list_services).post(create_service);
 
 export default router;

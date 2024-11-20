663cf41391063d4f19b88c10ad1572b2a0b18790
Rohit <rohitranjan2702@gmail.com>
2024-11-20 13:58:15 +0530

    refactore

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
-import {notFound,errorHandler} from './middleware/errorMiddleware';
-import path from 'path';
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
+import path from "path";
 
-import userRoutes from './routes/userRoutes';
-import resourceRoutes from './routes/resourceRoutes'
-const app=express();
+import userRoutes from "./routes/userRoutes";
+import resourceRoutes from "./routes/resourceRoutes";
+const app = express();
 dotenv.config();
-
 
 app.use(express.json()); // to accept JSON Data
 
-
-app.use('/api/user',userRoutes)
-app.use('/api/resource',resourceRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/resource", resourceRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-connectDB(()=>{
-    const server=app.listen(PORT,()=>{console.log(`Server Started on PORT ${PORT}`)});
+
+connectDB(() => {
+  const server = app.listen(PORT, () => {
+    console.log(`Server Started on PORT ${PORT}`);
+  });
 });
- 
-                
